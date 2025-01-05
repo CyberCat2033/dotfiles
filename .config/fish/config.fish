abda08b48e044be09c89dc97a1667f4af5c7d7a5
CyberCat <kkrulov2006@gmail.com>
2025-01-06 00:32:10 +0300

    Something

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 oh-my-posh init fish --config "~/.config/fish/rudolfs-dark.omp.json" | source
 export PKG_CONFIG_PATH=:/usr/lib/pkgconfig
-fastfetch
+fastfetch -l $FASTFETCH_LOGO_PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -11,4 +11,5 @@ set PATH $PATH /home/cybercat/.local/bin
 alias hyprconfig="cd ~/.config/hypr"
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 alias scripts='cd ~/.config/scripts/'
+alias :q='exit'
 # set -g theme_display_user yes
